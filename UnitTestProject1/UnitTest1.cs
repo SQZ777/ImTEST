8666c156f131218987ffdf926bd030d84ed8527e
sqz777In91 <sqz777isme@gmail.com>
2018-01-21 10:04:01 +0800

    pass fifteen love

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -11,13 +11,34 @@ namespace UnitTestProject1
             var tennisGame = new TennisGame();
             Assert.AreEqual("Love All", tennisGame.Score());
         }
+
+        [TestMethod]
+        public void Fifteen_Love()
+        {
+            var tennisGame = new TennisGame();
+            tennisGame.SetFirstPlayerScore(1);
+            Assert.AreEqual("Fifteen Love", tennisGame.Score());
+        }
+
     }
 
     public class TennisGame
     {
+        private int _firstPlayerScore;
+
         public string Score()
         {
+            if (_firstPlayerScore == 1)
+            {
+                return "Fifteen Love";
+            }
             return "Love All";
+        }
+
+        public void SetFirstPlayerScore(int i)
+        {
+
+            _firstPlayerScore = i;
         }
     }
 }
